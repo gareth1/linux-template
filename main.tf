2af401f5a01ced54a3733dcfd2f0f68cc03cc739
garethblakecoggins@googlemail.com <garethblakecoggins@googlemail.com>
2022-09-07 20:20:13 -0700

    added additional resource

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "i-ubuntu-linux-template" {
   ami                                 = "hardcode ami / or source latest from AWS"
   instance_type                       = "hardcode instance / or use variable"
   key_name                            = var.key_name
-  vpc_security_vpc_security_group_ids = [data.aws_security_group.all.id]
+  vpc_security_vpc_security_group_ids = [data.aws_security_group.template.id]
   subnet_id                           = local.subnets_by_az[count.index]
   associate_public_ip_address         = "true" 
 
@@ -22,23 +22,29 @@ resource "aws_instance" "i-ubuntu-linux-template" {
 
 #client access 
 
-ingress {
-    from_port  = 1812
-    to_port    = 1812
-    protocol   = "udp"
-    cidr_blocks = ["192.168.0.0/12"]
-}
+resource "aws_security_group" "template" {
+  for_each = { for g in var.groups : g.name => g }
 
-ingress {
-    from_port  = 1812
-    to_port    = 1812
-    protocol   = "tcp"
-    cidr_blocks = ["192.168.0.0/12"]
-}
+  name = "test2-${each.key}"
+  vpc_id = var.vpc_id
 
-egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = -1
-    cidr_blocks = ["0.0.0.0/0"]
- }
+  dynamic "ingress" {
+    for_each = var.tcp_ports
+    content {
+      from_port       = ingress.value
+      to_port         = ingress.value
+      protocol        = "tcp"
+      security_groups = var.groups[*].id
+    }
+  }
+
+  dynamic "ingress" {
+    for_each = var.udp_ports
+    content {
+      from_port       = ingress.value
+      to_port         = ingress.value
+      protocol        = "udp"
+      security_groups = var.groups[*].id
+    }
+  }
+}
